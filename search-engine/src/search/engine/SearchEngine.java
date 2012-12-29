13388e65f6d8e254645dfaef5e406b47d8f0b852
nunofaria11@gmail.com <nunofaria11@gmail.com@024ec359-9bad-2a39-02fd-1be574b367fc>
2012-12-29 15:20:22 +0000

    primeiro commit

---
+++ search-engine/src/search/engine/SearchEngine.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package search.engine;
+
+/**
+ *
+ * @author nuno
+ */
+public class SearchEngine {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // alterado pelo nuno
+    }
+}
