9d3d61e0291c6be808def9733fa1ab648e369714
nunofaria11@gmail.com <nunofaria11@gmail.com@024ec359-9bad-2a39-02fd-1be574b367fc>
2013-01-05 17:12:14 +0000

    RankingList

--- search-engine/src/search/engine/Engine.java
+++ search-engine/src/search/engine/Engine.java
@@ -4,9 +4,13 @@
  */
 package search.engine;
 
-import java.io.BufferedInputStream;
-import java.util.List;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
 
 /**
  *
@@ -14,33 +18,35 @@ import java.util.Scanner;
  */
 public class Engine {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws UnsupportedEncodingException, IOException {
 
+        Reader reader = new InputStreamReader(System.in, Charset.forName("UTF-8"));
+        BufferedReader stdin = new BufferedReader(reader);
 
-        Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         System.out.print("Caminho (default ENTER \".\"): ");
+
         // aqui tem que escrever "files/" 
-        String path = stdin.nextLine();
+        String path = stdin.readLine();
         if (path.equals("")) {
             path = ".";
         }
 
-
         System.out.print("Query: ");
-        String query = stdin.nextLine();
+        String query = stdin.readLine();
 
         ProcessorQC processor = new ProcessorQC(path, query);
-        
+
 
         BaseFormule bf = new BaseFormule();
-        List<String> filenames = processor.getFileNames(path);
-        int docCount = 0;
-        for (String filename : filenames) {
-            double docValue = bf.calculateValue(processor.getMatrixM(), processor.getMatrixQ(), docCount);
-            System.out.println("Documento \"" + filename + "\" = " + docValue);
-            docCount++;
-        }
+        ArrayList<String> filenames = processor.getFileNames(path);
+        double[][] M = processor.getMatrixM();
+        double[][] Q = processor.getMatrixQ();
 
+        RankingList rankingList = new RankingList(bf);
+
+        rankingList.createRankingList(M, Q, filenames);
+
+        rankingList.showRankingList();
 
 
     }
--- search-engine/src/search/engine/RankingList.java
+++ search-engine/src/search/engine/RankingList.java
@@ -4,10 +4,70 @@
  */
 package search.engine;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+
 /**
  *
  * @author nuno
  */
 public class RankingList {
-    
+
+    private IntFormule formula;
+    private ArrayList<String> rankingList;
+    private HashMap<String, Double> documentRanking;
+
+    public RankingList(IntFormule formula) {
+        this.formula = formula;
+    }
+
+    public void createRankingList(double[][] M, double[][] Q, ArrayList<String> filenames) {
+
+        int numDocs = filenames.size();
+
+        documentRanking = new HashMap<String, Double>();
+
+        for (int line = 0; line < numDocs; line++) {
+
+            double docValue = formula.calculateValue(M, Q, line);
+
+            documentRanking.put(filenames.get(line), docValue);
+
+        }
+
+        rankingList = new ArrayList<String>();
+
+        rankingList.addAll(documentRanking.keySet());
+
+        Comparator<String> rankingComparator = new Comparator<String>() {
+            @Override
+            public int compare(String doc1, String doc2) {
+                Double doc1Val = documentRanking.get(doc1);
+                Double doc2Val = documentRanking.get(doc2);
+                return doc1Val.compareTo(doc2Val);
+            }
+        };
+
+        /*
+         * ordenar a lista de documentos
+         */
+        Collections.sort(rankingList, rankingComparator);
+    }
+
+    /**
+     * Retorna a lista de documentos ordendados por ranking
+     *
+     * @return ArrayList<String> com os nomes dos documentos ordenados
+     */
+    public ArrayList<String> getRankingList() {
+        return rankingList;
+    }
+
+    public void showRankingList() {
+        for (String filename : rankingList) {
+            System.out.println(filename + " [ " + documentRanking.get(filename) + " ]");
+        }
+    }
 }
